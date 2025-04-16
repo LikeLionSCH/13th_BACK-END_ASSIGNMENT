f539bc577b04067a9505d2154ea2ff9269fc11ad
jhkim <no1tdc2@gmail.com>
2025-04-16 16:50:25 +0900

    week2last

--- KimJaeHwan/week1/LikeLionWeek1/src/main/java/com/jhkim0609/likelionweek1/controller/HelloController.java
+++ KimJaeHwan/week1/LikeLionWeek1/src/main/java/com/jhkim0609/likelionweek1/controller/HelloController.java
@@ -1,26 +1,24 @@
 package com.jhkim0609.likelionweek1.controller;
-
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.ArrayList;
 
 @RestController
 public class HelloController {
-    Map<String, Integer> map = new HashMap<>();
+    ArrayList<Pair<String, Integer>> myList = new ArrayList<Pair<String, Integer>>();
     @GetMapping("/hello")
-    public String test2(@RequestParam String name, @RequestParam Integer age) {
-        map.put(name, age);
-        return "name : " + name + "\nage : " + age;
+    public String hello(@RequestParam String name, @RequestParam Integer age) {
+        Pair<String, Integer> p = new Pair<String, Integer>(name, age);
+        this.myList.add(p);
+        return "name 은 " + name + "이고, 나이는 " + age;
     }
-    @GetMapping("/hello/{name}")
-    public String test3(@PathVariable String name) {
-        if (map.containsKey(name)) {
-            return name + "의 나이는 " + map.get(name);
-        }
-        return name + "은 저장되지 않은 사용자";
+    @GetMapping("/hello/{name1}")
+    public String print_age(@PathVariable("name1") String name1){
+        for (Pair<String, Integer> p : myList)
+            if(name1.equals(p.getFirst()))
+                return p.getSecond().toString();
+        return "저장되지 않은 사용자";
     }
 }
